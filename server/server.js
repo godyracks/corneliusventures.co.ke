2b1a3ed58b1c2cb8075b9c6db177dcca85254d0f
shubhth <godfreymatagaro@gmail.com>
2024-06-30 18:10:21 +0300

    hid password from public

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '*****',
-  database: process.env.DB_NAME || 'cvk',
+  database: process.env.DB_NAME || '****',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
