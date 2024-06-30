60553d2901d2f5d4a59e6fc1879dd4ac6317c65a
shubhth <godfreymatagaro@gmail.com>
2024-06-30 18:23:40 +0300

    updated authroutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,19 +2,16 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-// Google OAuth2 callback route
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
-    // Successful authentication, redirect or respond as needed
-    res.redirect('/'); // Redirect to homepage after successful login
+    res.redirect('/');
   }
 );
 
-// Example logout route
 router.get('/logout', (req, res) => {
   req.logout();
-  res.redirect('/'); // Redirect to homepage after logout
+  res.redirect('/');
 });
 
 module.exports = router;
