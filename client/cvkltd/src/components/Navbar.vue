41229c12575deae37b456fdc445622ca9748811e
shubhth <godfreymatagaro@gmail.com>
2024-07-01 14:47:25 +0300

    updated nav color

--- client/cvkltd/src/components/Navbar.vue
+++ client/cvkltd/src/components/Navbar.vue
@@ -68,7 +68,7 @@ export default {
   width: 100%;
   position: sticky;
   top: 0;
-  background-color: rgba(114, 160, 193, 0.8);
+  background-color: rgba(83, 92, 104, 0.8);
   z-index: 1000;
   padding: 10px 20px;
 }
