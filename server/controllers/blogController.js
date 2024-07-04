4869f0cf5b2f9de52dbc25a40fec1acb250e7daf
shubhth <godfreymatagaro@gmail.com>
2024-07-04 12:41:16 +0300

    updated blog controller to truncate words for preview

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -5,24 +5,38 @@ const multer = require('multer');
 const upload = require('../middleware/upload');
 
 exports.getBlogs = async (req, res) => {
-  try {
-    let sql = 'SELECT * FROM blog';
-    const limit = parseInt(req.query.limit);
-
-    if (!isNaN(limit) && limit > 0) {
+    try {
+      let sql = 'SELECT id, author, title, SUBSTRING_INDEX(content, " ", 20) AS content, image_url, date FROM blog';
+      const limit = parseInt(req.query.limit) || 3; // Default limit to 3 if not provided
+  
       sql += ` ORDER BY id DESC LIMIT ${limit}`;
-    } else {
-      sql += ` ORDER BY id DESC`;
+  
+      const [rows, fields] = await pool.query(sql);
+      res.json(rows);
+    } catch (error) {
+      console.error('Error fetching blogs:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
+  };
 
-    const [rows, fields] = await pool.query(sql);
-    res.json(rows);
-  } catch (error) {
-    console.error('Error fetching blogs:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
+  exports.getBlogById = async (req, res) => {
+    const blogId = req.params.id;
+  
+    try {
+      const [rows, fields] = await pool.query('SELECT * FROM blog WHERE id = ?', [blogId]);
+  
+      if (rows.length === 0) {
+        return res.status(404).json({ error: 'Blog not found' });
+      }
+  
+      const blog = rows[0];
+      res.json(blog);
+    } catch (error) {
+      console.error(`Error fetching blog with ID ${blogId}:`, error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+  
 exports.createBlog = async (req, res) => {
   const { author, title, content, date } = req.body;
   const imageUrl = req.file ? `uploads/${req.file.filename}` : null;
