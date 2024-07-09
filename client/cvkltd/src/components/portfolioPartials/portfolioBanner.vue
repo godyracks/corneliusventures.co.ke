4fa8f6988637702d4592dff3ef5341e5aa3a85d7
shubhth <godfreymatagaro@gmail.com>
2024-07-09 12:47:31 +0300

    added hero2 for portfolio

---
+++ client/cvkltd/src/components/portfolioPartials/portfolioBanner.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="portfolio-banner" :style="backgroundImageStyle">
+    <div class="overlay"></div>
+    <h1>Projects</h1>
+  </div>
+</template>
+
+<script>
+import backgroundImage from '@/assets/mansion_under.jpg';
+
+export default {
+  name: 'PortfolioBanner',
+  data() {
+    return {
+      backgroundImage
+    };
+  },
+  computed: {
+    backgroundImageStyle() {
+      return {
+        backgroundImage: `url(${this.backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '300px' /* Adjust the height as needed */
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.portfolio-banner {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  text-align: center;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 255, 0.5); /* Blue overlay with 50% opacity */
+  z-index: 1;
+}
+
+h1 {
+  position: relative;
+  z-index: 2;
+  font-size: 3em;
+}
+</style>
--- client/cvkltd/src/views/PortfolioView.vue
+++ client/cvkltd/src/views/PortfolioView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Navbar from '../components/common/Navbar.vue'
 import Images from '../components/portfolioPartials/portfolioImages.vue'
+import Hero2 from '../components/portfolioPartials/portfolioBanner.vue'
 import Footer from '../components/common/Footer.vue'
 
 </script>
@@ -8,6 +9,7 @@ import Footer from '../components/common/Footer.vue'
 <template>
   <main>
     <Navbar />
+      <Hero2 />
     <Images />
     <Footer />
   </main>
